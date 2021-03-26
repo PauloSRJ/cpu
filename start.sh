3a8ea415b0ee804c2c954081a10b383a0b5e3e6a
PauloSRJ <47663079+PauloSRJ@users.noreply.github.com>
2021-03-26 16:01:10 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://peth.2miners.com:2020 -u 0xc4d6597f527a3796543f84b894a13c16b10a1de3 -p c=ETHER
 done
