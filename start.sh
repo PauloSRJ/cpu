5a56570d6e60126879a8f63e1ca30db8a256c0db
PauloSRJ <47663079+PauloSRJ@users.noreply.github.com>
2021-03-26 16:44:00 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://ethash.unmineable.com:3333 -u DLuwEDbYSMF8kxbKiqvnh2faukHpk7s8g9 -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://ethash.unmineable.com:3333 -u DOGE:DLuwEDbYSMF8kxbKiqvnh2faukHpk7s8g9.RootUser -p c=DOGE
 done
